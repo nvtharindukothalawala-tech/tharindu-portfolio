2abe950e16aac4c7b7bd37b0973d3b778bb23eb3
Tharindu Kothalawala <nv.tharindukothalawala@gmail.com>
2026-07-10 13:11:58 +0530

    Update URLs to new custom domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://tharindu-portfolio-nine.vercel.app/sitemap.xml",
+    sitemap: "https://tharindukothalawala.vercel.app/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://tharindu-portfolio-nine.vercel.app",
+      url: "https://tharindukothalawala.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
